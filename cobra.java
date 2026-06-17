e8be9ad8781aecbb10a6dd241d1fc0c82b0a312e
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-06-17 08:08:32 +0530

    learning variable

--- cobra.java
+++ cobra.java
@@ -12,11 +12,22 @@
 // }
 
 
+// public class cobra{
+//     public static void main(String [] args){
+//         System.out.println("hello soumya");
+//         System.out.println("hello sir");
+//         System.out.println("how are you");
+//         System.out.println("nothing to show");
+//         System.out.println("hello soumyadeep");
+//     }
+// }
+
+
 public class cobra{
-    public static void main(String [] args){
-        System.out.println("hello soumya");
-        System.out.println("hello sir");
-        System.out.println("how are you");
-        System.out.println("nothing to show");
+    public static void main(String []args){
+        String name = "aman";
+        String area_name = "Daspur";
+        System.out.println(name);
+        System.out.println(area_name);
     }
 }
