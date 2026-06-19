244aaeff454d17f2218a3c0e6001f76799c8f1f7
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-06-19 21:09:34 +0530

    day 4

---
+++ data.java
@@ -0,0 +1,12 @@
+public class data{
+    public static void main(String[]args){
+        String name = "soumyadeep";
+        int age = 12;
+        String friend = name;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println("my name is :" + name);
+        System.out.println("my age is :" + age);
+        System.out.println("also my friend is : " + friend);
+    }
+}
---
+++ datatype.java
@@ -0,0 +1,20 @@
+public class datatype{
+    public static void main(String[] args){
+        int phone = 1234567891;
+        byte num = 125;
+        long mony = 12345678910111L;
+        float taka = 3.4F;
+        char name = 'a';
+        boolean adult = true;
+
+        System.out.println("my phone nuber is:" + phone);
+        System.out.println("byte nuber is :" + num);
+
+        System.out.println("how much mony is:" + mony);
+        System.out.println("my taka is" + taka);
+        System.out.println("name of charecter is" + name);
+        System.out.println("he is a adult person" + adult);
+
+
+    }
+}
