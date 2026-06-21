7055213e4673fcd99b9a94e04b8a915e3cf40509
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-06-21 23:00:27 +0530

    compleate day 6

---
+++ day6.java
@@ -0,0 +1,13 @@
+public class day6{
+    public static void main(String[] args){
+        String name ="soumyadeep";
+        String friend = "john";
+        String name2 = name + " king" + friend;
+    
+        System.out.println(name2);
+        System.out.println(name2.charAt(1));
+        System.out.println(name.replace("s", "g"));
+        System.out.println(name.length());
+        System.out.println(name.substring(0,3));
+    }
+}
