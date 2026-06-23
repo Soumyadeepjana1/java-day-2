1b96ebc95ff230985917b96826738f5b1850bbe6
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-06-23 22:19:28 +0530

    practice done

---
+++ practice.java
@@ -0,0 +1,72 @@
+// // q 1
+// public class practice {
+//     public static void main(String[] args) {
+//         int[] element = new int[5];
+//         element[0] = 10;
+//         element[1] = 20;
+//         element[2] = 30;
+//         element[3] = 40;
+//         element[4] = 50;
+
+//         System.out.println(element[0]);
+//         System.out.println(element[1]);
+//         System.out.println(element[2]);
+//         System.out.println(element[3]);
+//         System.out.println(element[4]);
+//     }
+// }
+
+// q -2
+
+// public class practice {
+//     public static void main(String[] args) {
+//         int[] element = new int[4];
+//         element[0] = 85;
+//         element[1] = 90;
+//         element[2] = 78;
+//         element[3] = 95;
+// //         System.out.println("first element is" + element[0]);
+// //         System.out.println("last element is " + element[3]);
+//      }
+// }
+
+// q-3
+
+// public class practice {
+//     public static void main(String[] args) {
+//         int[] marks = new int[5];
+//         marks[0] = 90;
+//         marks[1] = 75;
+//         marks[2] = 85;
+//         marks[3] = 87;
+//         marks[4] = 82;
+
+//         System.out.println(marks.length);
+//     }
+// }
+
+// // q-4
+
+// public class practice {
+//     public static void main(String[] args) {
+//         int[] marks = new int[5];
+//         marks[0] = 80;
+//         marks[1] = 85;
+//         marks[2] = 90;
+//         marks[3] = 75;
+//         marks[4] = 95;
+
+//         System.out.println(marks[0] + marks[1] + marks[2] + marks[3] + marks[4]);
+//     }
+// }
+
+// q -5
+import java.util.Arrays;
+
+public class practice {
+    public static void main(String[] args) {
+        int[] array = { 12, 45, 7, 89, 34 };
+        Arrays.sort(array);
+        System.out.println(array[4]);
+    }
+}
