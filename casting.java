2f08ff7e5f43d73d407f1ab3755e422ca1e83f18
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-06-25 22:26:28 +0530

    day 9

--- casting.java
+++ casting.java
@@ -16,21 +16,29 @@
 //     }
 // }
 
-public class casting {
-    public static void main(String[] args) {
-        float kg = 12.5F;
-        int less = (int) kg;
-        System.out.println(kg);
-        System.out.println(less);
-    }
-}
+// public class casting {
+//     public static void main(String[] args) {
+//         float kg = 12.5F;
+//         int less = (int) kg;
+//         System.out.println(kg);
+//         System.out.println(less);
+//     }
+// }
+
+// public class casting {
+//     public static void main(String[] args) {
+//         byte value = 100;
+//         double vb = (double) value;
+//         System.out.println(value);
+//         System.out.println(vb);
+//     }
+
+// }
 
 public class casting {
     public static void main(String[] args) {
-        byte value = 100;
-        double vb = (double) value;
-        System.out.println(value);
-        System.out.println(vb);
+        int year = 22;
+        double age_and_month = 5 + (double) year;
+        System.out.println("john age is " + year + (" year ") + age_and_month + (" month"));
     }
-
 }
---
+++ constant.java
@@ -0,0 +1,6 @@
+public class constant {
+    public static void main(String[] args) {
+        final int date_of_birth = 2004;
+        System.out.println(date_of_birth);
+    }
+}
