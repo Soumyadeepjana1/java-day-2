4013816824e0288bc2c506bad495e70142d406e8
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-06-27 23:24:45 +0530

    practice done

--- practice2.java
+++ practice2.java
@@ -9,13 +9,30 @@
 //     }
 // }
 
+// public class practice2 {
+//     public static void main(String[] args) {
+//         double age = 300.5;
+//         final String name = "soumyadeep";
+//         int age1 = (int) age;
+//         System.out.println(age);
+//         System.out.println(age1);
+//         System.out.println(name);
+//     }
+// }
+import java.util.Arrays;
+
 public class practice2 {
     public static void main(String[] args) {
-        double age = 300.5;
-        final String name = "soumyadeep";
-        int age1 = (int) age;
-        System.out.println(age);
-        System.out.println(age1);
-        System.out.println(name);
+        int[] name = { 12, 14, 15, 16 };
+        double name1 = 12.5;
+        int[][] friends = { { 16, 19, 13, 19 }, { 12, 9, 17, 14, 16 } };
+        int no = (int) name1;
+        System.out.println("print " + name.length);
+        Arrays.sort(name);
+        System.out.println(name[3]);
+        System.out.println("print" + name[0]);
+        System.out.println(friends[0][3]);
+        System.out.println(friends.length);
+        System.out.println("print" + no);
     }
 }
