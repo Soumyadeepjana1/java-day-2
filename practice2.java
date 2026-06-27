e254cccfd8f5d5209c9af8bf3ce4c65c4f4f73e0
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-06-27 20:40:33 +0530

    add

--- practice2.java
+++ practice2.java
@@ -1,3 +1,14 @@
+// public class practice2 {
+//     public static void main(String[] args) {
+//         double age = 300.5;
+//         final String name = "soumyadeep";
+//         int age1 = (int) age;
+//         System.out.println(age);
+//         System.out.println(age1);
+//         System.out.println(name);
+//     }
+// }
+
 public class practice2 {
     public static void main(String[] args) {
         double age = 300.5;
