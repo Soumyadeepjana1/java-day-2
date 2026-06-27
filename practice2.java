624538953de1cc5fdb62460a4b103b0fcd7247cb
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-06-27 23:31:05 +0530

    done

--- practice2.java
+++ practice2.java
@@ -26,6 +26,8 @@ public class practice2 {
         int[] name = { 12, 14, 15, 16 };
         double name1 = 12.5;
         int[][] friends = { { 16, 19, 13, 19 }, { 12, 9, 17, 14, 16 } };
+        int value1 = 12;
+        int value2 = 13;
         int no = (int) name1;
         System.out.println("print " + name.length);
         Arrays.sort(name);
@@ -34,5 +36,6 @@ public class practice2 {
         System.out.println(friends[0][3]);
         System.out.println(friends.length);
         System.out.println("print" + no);
+        System.out.println("plus of two number is : " + (value1 + value2));
     }
 }
