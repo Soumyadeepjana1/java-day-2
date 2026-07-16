674cd90b09189d4f57ea558da19dfc09ecb5d32e
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-07-16 19:58:20 +0530

    loop done

---
+++ w1.java
@@ -0,0 +1,23 @@
+public class w1 {
+    public static void main(String[] args) {
+        int i = 100;
+        int num = 7;
+        int k = 0;
+        {
+            while (i >= 1) {
+                if (num % 2 == 0)
+                    System.out.print("khankir 6ele");
+                else
+                    System.out.print("randir 6ele  ");
+
+                i--;
+            }
+
+            do {
+                System.out.println(k);
+                k++;
+            } while (k <= 100);
+        }
+
+    }
+}
