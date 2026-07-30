a559c56c6d49e8f0265a054b24231965163e355a
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-07-30 21:06:11 +0530

    add

---
+++ length.java
@@ -0,0 +1,8 @@
+public class length {
+    public static void main(String[] args) {
+        String name = "soumyadeep";
+        String john = "hi my name is soumyadeep jana how are you";
+        System.out.println(name.length());
+        System.out.println(john.length());
+    }
+}
