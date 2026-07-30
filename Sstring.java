9eb56d06605b62a55b627c63e30c633a38de200a
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-07-30 21:12:54 +0530

    add string

---
+++ Sstring.java
@@ -0,0 +1,9 @@
+public class Sstring {
+    public static void main(String[] args) {
+        String name = "soumyadeep";
+        String surname = "jana";
+        String ns = name + surname;
+        System.out.println(ns);
+
+    }
+}
