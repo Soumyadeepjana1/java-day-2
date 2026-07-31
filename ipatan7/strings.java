1e893bd02ac901855792b4293915350be20b68e6
ipatan7 <iqbalpatan083@gmail.com>
2026-07-31 15:24:01 +0530

    added strings program

---
+++ ipatan7/strings.java
@@ -0,0 +1,8 @@
+public class strings {
+
+    public static void main(String[] args) {
+
+        String UID = "24MCC20081";
+        System.out.println("the leght of your UID is" + UID.length());
+    }
+}
