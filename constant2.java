cbc990039bf44ca27e0ae60234f702d5bf78f194
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-08-01 23:29:10 +0530

    add all

--- constant2.java
+++ constant2.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class constant2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number ");
+        int a = sc.nextInt();
+        System.out.println("number is " + a);
         int num = 12;
         num = 34;
-        final int a = 36; // constant (using final key word)
+        final int b = 36; // constant (using final key word)
 
         System.out.println(num);
-        System.out.println(a);
+        System.out.println(b);
+        sc.close();
     }
 }
---
+++ ifelse2.java
@@ -0,0 +1,13 @@
+public class ifelse2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        if (a > b)
+            System.out.println("a is bigger the b");
+        else if (b > a)
+            System.out.println("b is bigger the a");
+        else
+            System.out.println("number is natural");
+    }
+}
