5b21a637b4d7b139f0702f2c228c6a1ac8761e11
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-08-01 21:37:20 +0530

    add

---
+++ twodarray.java
@@ -0,0 +1,9 @@
+public class twodarray {
+    public static void main(String[] args) {
+        int[][] age = { { 1, 23, 4, 6 }, { 12, 45, 16, 23 } };
+        System.out.println(age[0][0]);
+        System.out.println(age[1][0]);
+
+    }
+
+}
