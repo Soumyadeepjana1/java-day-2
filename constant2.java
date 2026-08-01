f9234e1be92a6935a05ea88c2d9d40068201051b
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-08-01 23:15:54 +0530

    add all

--- constant2.java
+++ constant2.java
@@ -2,7 +2,7 @@ public class constant2 {
     public static void main(String[] args) {
         int num = 12;
         num = 34;
-        final int a = 36;
+        final int a = 36; // constant (using final key word)
 
         System.out.println(num);
         System.out.println(a);
---
+++ math.java
@@ -0,0 +1,9 @@
+public class math {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.min(a, b));
+        System.out.println((int) Math.random() * 100);
+    }
+}
