f62d2e4653d801e6637df379551389c1808b3e8b
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-08-01 22:06:37 +0530

    add all

---
+++ constant2.java
@@ -0,0 +1,10 @@
+public class constant2 {
+    public static void main(String[] args) {
+        int num = 12;
+        num = 34;
+        final int a = 36;
+
+        System.out.println(num);
+        System.out.println(a);
+    }
+}
