4dc4f3d1ec5c0e581c0209b5bbc7781043b20223
ipatan7 <iqbalpatan083@gmail.com>
2026-08-01 18:46:00 +0530

    first array program

---
+++ ipatan7/array.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+
+        String[] cars = { "BMW", "Mercedez Benz", "Audi" };
+        int[] models = { 2002, 2004, 2018, 2016 };
+        System.out.println(Arrays.toString(cars));
+        System.out.println(Arrays.toString(models));
+
+    }
+}
