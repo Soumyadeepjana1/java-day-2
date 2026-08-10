d6959326bb3e5b2e6b11a93148713c5cd43898e1
ipatan7 <iqbalpatan083@gmail.com>
2026-08-10 18:37:19 +0530

    methods

---
+++ ipatan7/fname.java
@@ -0,0 +1,11 @@
+public class fname {
+    static void mymethod(String fname) {
+        System.out.println(fname + "patan ");
+    }
+
+    public static void main(String[] args) {
+        mymethod("Omar");
+        mymethod("Abubaker");
+        mymethod("Usman");
+    }
+}
