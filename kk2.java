f66786306c76de696d098f31ab8e1c8bb2ef3d88
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-09-01 23:02:48 +0530

    new commit

---
+++ kk2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class kk2 {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         int a = sc.nextInt();
+
+//         System.out.println(a);
+//     }
+// }
+
+   public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println(a);
+   }
+
+}
---
+++ kk3.java
@@ -0,0 +1,15 @@
+public class kk3{
+    public static void main(String [] args){
+        int a = 20;
+        float b = 22.4F;
+        double c = 122222.56777;
+        long d = 1234556777;
+        char t = 'c';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(t);
+    }
+}
