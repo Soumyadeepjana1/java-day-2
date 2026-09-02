426696dca61e77fe20ad38f11702177f99968696
Soumyadeep jana <soumyadeepjana368@gmail.com>
2026-09-02 10:20:55 +0530

    new commit

---
+++ evenodd.java
@@ -0,0 +1,12 @@
+public class evenodd {
+    public static void  main(String [] args){
+        for(int i = 0; i <= 20; i++){
+            if(i % 2 == 0){
+                System.out.println("number is even "+ i);
+            }
+            else{
+                System.out.println("number is odd "+ i);
+            }
+        }
+    }
+}
